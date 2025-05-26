ff95eb862a1f2ed6e8c2593549d474d28503266e
carlynxd <carlos.ln.freitas@gmail.com>
2025-05-26 20:28:59 -0300

    Ajustes no banco de dados

--- database/banco.sql
+++ database/banco.sql
@@ -1,3 +1,19 @@
+CREATE TABLE tb_curso 
+( 
+ id_curso INT PRIMARY KEY AUTO_INCREMENT,  
+ nome_curso VARCHAR(255) NOT NULL,   
+ qntd_alunos INT NOT NULL  
+)
+
+CREATE TABLE tb_aluno 
+( 
+ id_aluno INT PRIMARY KEY AUTO_INCREMENT,  
+ nome VARCHAR(255) NOT NULL,  
+ notas VARCHAR(255) NOT NULL,
+ id_curso INT NOT NULL,  
+    FOREIGN KEY (id_curso) REFERENCES tb_curso(id_curso) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
 CREATE TABLE tb_cadastro 
 ( 
  Nome VARCHAR(255) NOT NULL,  
@@ -6,28 +22,20 @@ CREATE TABLE tb_cadastro
  cep VARCHAR(10) NOT NULL,
  data_nasc DATE NOT NULL,
  id_curso INT NOT NULL,
-    FOREIGN KEY (id_curso) REFERENCES tb_curso(id_curso) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_curso) REFERENCES tb_curso(id_curso) ON DELETE RESTRICT ON UPDATE CASCADE,
  id_aluno INT NOT NULL,  
-    FOREIGN KEY (id_aluno) REFERENCES tb_aluno(id_aluno) ON DELETE CASCADE ON UPDATE CASCADE
-);  
+    FOREIGN KEY (id_aluno) REFERENCES tb_aluno(id_aluno) ON DELETE RESTRICT ON UPDATE CASCADE
+)
 
-CREATE TABLE tb_curso 
+CREATE TABLE tb_turma 
 ( 
- id_curso INT PRIMARY KEY AUTO_INCREMENT,  
- nome_curso VARCHAR(255) NOT NULL,   
- qntd_alunos INT NOT NULL,  
+ id_turma INT PRIMARY KEY AUTO_INCREMENT,  
+ nome_turma VARCHAR(255) NOT NULL,  
+ periodo VARCHAR(50) NOT NULL,
+ id_curso int,
+    FOREIGN KEY (id_curso) REFERENCES tb_curso(id_curso) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE TABLE tb_aluno 
-( 
- id_aluno INT PRIMARY KEY AUTO_INCREMENT,  
- nome VARCHAR(255) NOT NULL,  
- notas VARCHAR(255) NOT NULL
- id_cadastro INT NOT NULL,  
-    FOREIGN KEY (id_cadastro) REFERENCES tb_cadastro(id_cadastro) ON DELETE CASCADE ON UPDATE CASCADE,
- id_curso INT NOT NULL,  
-    FOREIGN KEY (id_curso) REFERENCES tb_curso(id_curso) ON DELETE CASCADE ON UPDATE CASCADE
-);
 CREATE TABLE tb_professor 
 ( 
  id_prof INT PRIMARY KEY AUTO_INCREMENT,  
@@ -37,12 +45,7 @@ CREATE TABLE tb_professor
  id_turma INT NOT NULL,  
     FOREIGN KEY (id_turma) REFERENCES tb_turma(id_turma) ON DELETE CASCADE ON UPDATE CASCADE
 );
-CREATE TABLE tb_turma 
-( 
- id_turma INT PRIMARY KEY AUTO_INCREMENT,  
- nome_turma VARCHAR(255) NOT NULL,  
- periodo VARCHAR(50) NOT NULL
-);
+
 CREATE TABLE tb_empresa 
 ( 
  id_empresa INT PRIMARY KEY AUTO_INCREMENT,  
@@ -50,6 +53,7 @@ CREATE TABLE tb_empresa
  cnpj VARCHAR(18) NOT NULL,  
  endereco VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE tb_projetos
 ( 
  id_projeto INT PRIMARY KEY AUTO_INCREMENT,  
@@ -57,8 +61,8 @@ CREATE TABLE tb_projetos
  data_inicio DATE NOT NULL,  
  data_fim DATE NOT NULL,  
  id_prof INT NOT NULL,  
-    FOREIGN KEY (id_prof) REFERENCES tb_professor(id_prof) ON DELETE CASCADE ON UPDATE CASCADE,
-);
+    FOREIGN KEY (id_prof) REFERENCES tb_professor(id_prof) ON DELETE CASCADE ON UPDATE CASCADE
+)
 
 DROP TABLE tb_professor;
 DROP TABLE tb_turma;
